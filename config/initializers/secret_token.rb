a66a3a03320d9dffefde476202c2009c8626681b
version <gem install sqlite-3ruby>
2014-03-20 17:52:14 +0100

    First Commit Jeroens Magic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog0320::Application.config.secret_key_base = '0f4df4237a4f71f6275bbfdc3d80d550b67f5a668d364c7bcf7730d48b9ddfb96731a2d55aa6fd70d5946df86076fdb4b8abf099c80988f63128996db871194d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blog0320::Application.config.session_store :cookie_store, key: '_blog0320_session'
